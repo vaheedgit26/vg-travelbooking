44bf05eff4bcad2780ef75f9ade2b4b14acacabc
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-29 06:48:03 +0530

    Update main.tf

--- infra/30-ecr/main.tf
+++ infra/30-ecr/main.tf
@@ -1,10 +1,14 @@
 module "ecr" {
   source = "git::https://github.com/vaheedgit26/Infra-1.0.git//modules/ecr"
 
-  project = var.project   # "shopverse"
+  project = var.project   # "travelbooking"
   env     = var.env       # "dev"
   repositories = [
-    "shopverse-frontend",
-    "shopverse-backend"
+    "forntend-service",
+    "user-service",
+    "search-service",
+    "booking-service",
+    "payment-service",
+    "notification-service"
   ]
 }
