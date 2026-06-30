3f987aa86d20145414342ead8451fa2aa93763d7
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-30 20:29:08 +0530

    Update data.tf

--- infra/20-rds/data.tf
+++ infra/20-rds/data.tf
@@ -1,8 +1,8 @@
 # Use existing AWS Secrets Manager Secret (which is already created)
-data "aws_secretsmanager_secret" "shopverse_secret" {
+data "aws_secretsmanager_secret" "travelbooking_secret" {
   name = var.aws_secret_name
 }
 
-data "aws_secretsmanager_secret_version" "shopverse_secret_value" {
-  secret_id = data.aws_secretsmanager_secret.shopverse_secret.id
+data "aws_secretsmanager_secret_version" "travelbooking_secret_value" {
+  secret_id = data.aws_secretsmanager_secret.travelbooking_secret.id
 }
