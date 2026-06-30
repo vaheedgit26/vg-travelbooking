77022779a43bc537a695e06c938de8a10414199f
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-30 20:31:38 +0530

    Update locals.tf

--- infra/20-rds/locals.tf
+++ infra/20-rds/locals.tf
@@ -1,12 +1,12 @@
 locals {
   resource_name = "${var.project}-${var.env}"
  
-  availability_zone      = data.terraform_remote_state.vpc.outputs.availability_zones[0]
-  sg_name                = "${local.resource_name}-mysql-rds-sg"
-  identifier             = "${local.resource_name}-mysql"
-  db_subnet_group_name   = "${local.resource_name}-mysql-rds-db-subnet-group"
-  shopverse_secret_json  = jsondecode(data.aws_secretsmanager_secret_version.shopverse_secret_value.secret_string)
-  vpc_security_group_ids = [data.terraform_remote_state.eks.outputs.cluster_security_group_id, data.terraform_remote_state.bastion.outputs.bastion_sg_id]
+  availability_zone          = data.terraform_remote_state.vpc.outputs.availability_zones[0]
+  sg_name                    = "${local.resource_name}-postgre-rds-sg"
+  identifier                 = "${local.resource_name}-postgre"
+  db_subnet_group_name       = "${local.resource_name}-postgre-rds-db-subnet-group"
+  travelbooking_secret_json  = jsondecode(data.aws_secretsmanager_secret_version.travelbooking_secret_value.secret_string)
+  vpc_security_group_ids     = [data.terraform_remote_state.eks.outputs.cluster_security_group_id, data.terraform_remote_state.bastion.outputs.bastion_sg_id]
 
   common_tags = {
     Project     = var.project
