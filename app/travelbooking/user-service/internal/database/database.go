953b1d2a28ffd99264ac3d0284579ebd304ef810
vaheedgit26 <vaheed.aws@gmail.com>
2026-07-02 22:01:12 +0530

    Update database.go

--- app/travelbooking/user-service/internal/database/database.go
+++ app/travelbooking/user-service/internal/database/database.go
@@ -3,18 +3,24 @@ package database
 import (
 	"fmt"
 	"user-service/internal/config"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"os"
 )
 
 var DB *gorm.DB
 
 func Connect(cfg *config.Config) error {
+	sslMode := os.Getenv("DB_SSLMODE")
+	
+    if sslMode == "" {
+        sslMode = "require"
+    }
+	
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=UTC",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, sslMode,
 	)
 
 	gormLogger := logger.Default.LogMode(logger.Silent)
